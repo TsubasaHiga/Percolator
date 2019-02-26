85930f9442ba6647c943ce47197dfbb042289cb6
TsubasaHiga <info@higatsubasa.com>
2019-02-26 18:48:07 +0900

    babel追加

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,30 @@
     development に設定するとソースマップ有効
 *
 * -------------------------------------------------------------*/
+const webpack = require('webpack');
 const config  = {
     watch   : true,
     mode    : "development",
     entry   : "./src/js/main.js",
     output  : {
         filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [
+                                ['@babel/preset-env', {'modules': false}]
+                            ]
+                        }
+                    }
+                ]
+            }
+        ]
     }
 };
 
